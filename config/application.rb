b5064c8e25f1150be93f0fa2de2a90c462ca8ee6
Fish-bowl <symbolt.ap@gmail.com>
2018-10-16 10:26:23 -0600

    stubbed out comps

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Yahtzee
     config.middleware.insert_before 0, Rack::Cors do 
       allow do 
         origins '*'
-        resources '*',
+        resource '*',
           methods: [:get, :post, :put, :delete],
           expose: ['access-token', 'client', 'uid', 'expiry']
       end 
